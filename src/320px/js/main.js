84a8fe09c9bbcfb81707dcd00aac327ccdc7074d
Atsushi <pipos.pipopo@gmail.com>
2021-01-29 01:58:29 +0900

    ディレクトリ設定

--- src/320px/js/main.js
+++ src/320px/js/main.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     // ページ読み込み時に実行したい処
     $('#resolution').text("解像度：" + window.screen.availWidth);
-    $.getJSON('../json/words.json', (data) => {
+    $.getJSON('../../json/words.json', (data) => {
         console.log(data);
         for (var item in data) {
             console.log(data[item]);
