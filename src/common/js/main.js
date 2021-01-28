dea0e1a7a9714490d3e7214740d048835dc62ad0
Atsushi <pipos.pipopo@gmail.com>
2021-01-29 02:39:45 +0900

    indexにjsを読み込むのは不要だったので削除　ローカルで参照するとき用にリンクを追加

--- src/common/js/main.js
+++ src/common/js/main.js
@@ -1,8 +1,9 @@
 window.onload = function () {
     // ページ読み込み時に実行したい処
     $('#resolution').text("解像度：" + window.screen.availWidth);
+    // ローカルで見るとき用のリンク
+    // $.getJSON('https://nukegarapipo.github.io/GitHubWebPages/src/common/json/words.json', (data) => {
     $.getJSON('../common/json/words.json', (data) => {
-        console.log(data);
         for (var item in data) {
             console.log(data[item]);
             console.log(data[item]['size']);
