b3683baa75f50430254cd06101e1e426ed30dd41
Atsushi <pipos.pipopo@gmail.com>
2021-01-29 02:18:13 +0900

    ワードクラウドの処理追加

--- src/common/js/main.js
+++ src/common/js/main.js
@@ -10,3 +10,45 @@ window.onload = function () {
         layout(data);
     });
 }
+
+// グラフの表示設定
+var width = 320;
+var height = 320;
+
+function layout(words) {
+    for (var i = 0; i < words.length; i++) {
+        words[i]['size'] *= 15;
+    }
+
+    d3.layout.cloud()
+        .size([width, height])
+        // wordsメソッドに送るwordsを整形している
+        .words(words.map(d => { return { "text": d['text'], "size": d['size'], "url": d['url'] }; }))
+        .padding(2)
+        .rotate(() => ~~Math.random() * 2)
+        .font("Impact")
+        .fontSize(d => d.size)
+        .on("end", draw)
+        .start();
+}
+
+// layoutの出力を受け取り単語を描画
+function draw(words) {
+    d3.select("#wordcloud")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+        .selectAll("text")
+        .data(words)
+        .enter().append("text")
+        .style("font-size", d => d.size)
+        .style("font-family", "Impact")
+        .style("fill", (d, i) => d3.schemeSet2[i % 10])
+        .attr("text-anchor", "middle")
+        .style("cursor", "pointer")
+        .attr("transform", d => "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")")
+        .text(d => d.text)
+        .on("click", (d, i) => window.open(d.url, "_blank"));
+}
