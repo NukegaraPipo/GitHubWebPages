e9d984f4e14c807b4032aea27112984aea406183
Atsushi <pipos.pipopo@gmail.com>
2021-01-29 02:58:33 +0900

    ラムダ式の辞書がどうやらd3の処理内だと.でつないで書けるみたい　あと、やはりhtml側でjsを呼び出した時はhtmlのファイルパスを起点に処理がなされるみたいなので jsファイルには「../common/json/words.json」と書くのが正しいみたい

--- src/common/js/main.js
+++ src/common/js/main.js
@@ -18,13 +18,13 @@ var height = 320;
 
 function layout(words) {
     for (var i = 0; i < words.length; i++) {
-        words[i]['size'] *= 15;
+        words[i]['size'] *= 1.1;
     }
 
     d3.layout.cloud()
         .size([width, height])
         // wordsメソッドに送るwordsを整形している
-        .words(words.map(d => { return { "text": d['text'], "size": d['size'], "url": d['url'] }; }))
+        .words(words.map(d => { return { "text": d.text, "size": d.size, "url": d.url }; }))
         .padding(2)
         .rotate(() => ~~Math.random() * 2)
         .font("Impact")
@@ -46,10 +46,10 @@ function draw(words) {
         .enter().append("text")
         .style("font-size", d => d.size)
         .style("font-family", "Impact")
-        .style("fill", (d, i) => d3.schemeSet2[i % 10])
+        .style("fill", () => d3.schemeSet2[Math.floor(Math.random() * 10) % 7])
         .attr("text-anchor", "middle")
         .style("cursor", "pointer")
         .attr("transform", d => "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")")
         .text(d => d.text)
-        .on("click", (d, i) => window.open(d.url, "_blank"));
+        .on("click", d => window.open(d.url, "_blank"));
 }
