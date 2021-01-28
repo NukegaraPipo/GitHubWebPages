92cd876286b15eca6215ac74053d2832bc30da17
Atsushi <pipos.pipopo@gmail.com>
2021-01-29 01:55:15 +0900

    gitpagesの階層設定

---
+++ src/320px/js/main.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    // ページ読み込み時に実行したい処
+    $('#resolution').text("解像度：" + window.screen.availWidth);
+    $.getJSON('../json/words.json', (data) => {
+        console.log(data);
+        for (var item in data) {
+            console.log(data[item]);
+            console.log(data[item]['size']);
+        }
+        layout(data);
+    });
+}
---
+++ src/720px/js/main.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+    // ページ読み込み時に実行したい処
+    $('#resolution').text("解像度：" + window.screen.availWidth);
+}
