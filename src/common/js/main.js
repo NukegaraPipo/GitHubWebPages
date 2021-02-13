f2c396268f118b871e76d8caf32da4834db55423
Atsushi <pipos.pipopo@gmail.com>
2021-02-13 17:10:47 +0900

    path

--- src/common/js/main.js
+++ src/common/js/main.js
@@ -2,17 +2,37 @@ window.onload = function () {
 
     var pixelResolutionWidth = window.screen.width * devicePixelRatio;
 
-    var url = location.href;
     var path = location.pathname;
-
-    var ida = location.search;
-
     var sp = location.href.split("/");
 
-    console.log("location：" + url);
-    console.log("patu：" + path);
-    var pathName = sp[sp.length - 2]
-    console.log(pathName);
-    var link = '/src/720px/tab01.html';
+    console.log("path：" + path);
+    console.log("href：" + location.href);
 
+    var pathName = sp[sp.length - 2];
+    console.log(pathName);
+
+    var link = "";
+    //     if (1280 <= pixelResolutionWidth) {
+    //         // 現在のページが1280px以外だったら
+    //         if (pathName != "1280px") {
+    //             var userAgent = window.navigator.userAgent.toLowerCase();
+    //             if (userAgent.indexOf("mobile") != -1) {
+    //                 link = 'src/720px/tab01.html';
+    //             }
+    //             else {
+    //                 link = 'src/1280px/tab01.html';
+    //             }
+    //         }
+    //     }
+    //     else if (720 <= pixelResolutionWidth) {
+    //         if (pathName != "720px") {
+    //             link = 'src/720px/tab01.html';
+    //         }
+    //     }
+    //     else {
+    //         if (pathName != "320px") {
+    //             link = 'src/320px/tab01.html';
+    //         }
+    //     }
+    //     location.pathname = "../";
 };
