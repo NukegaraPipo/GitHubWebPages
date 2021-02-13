c6ce1840523efe1b73f795f3918f24de331d3b40
Atsushi <pipos.pipopo@gmail.com>
2021-02-14 01:33:36 +0900

    weight margin 調整

--- src/common/js/main.js
+++ src/common/js/main.js
@@ -3,29 +3,29 @@ window.addEventListener('DOMContentLoaded', function () {
     var sp = location.href.split("/");
     var pathName = sp[sp.length - 2];
 
-    if (1280 <= pixelResolutionWidth) {
-        if (pathName != "1280px") {
-            var userAgent = window.navigator.userAgent.toLowerCase();
-            if (userAgent.indexOf("mobile") != -1) {
-                sp[sp.length - 2] = "720px";
-                location.href = sp.join('/');;
-            }
-            else {
-                sp[sp.length - 2] = "1280px";
-                location.href = sp.join('/');;
-            }
-        }
-    }
-    else if (720 <= pixelResolutionWidth) {
-        if (pathName != "720px") {
-            sp[sp.length - 2] = "720px";
-            location.href = sp.join('/');;
-        }
-    }
-    else {
-        if (pathName != "320px") {
-            sp[sp.length - 2] = "320px";
-            location.href = sp.join('/');;
-        }
-    }
+    // if (1280 <= pixelResolutionWidth) {
+    //     if (pathName != "1280px") {
+    //         var userAgent = window.navigator.userAgent.toLowerCase();
+    //         if (userAgent.indexOf("mobile") != -1) {
+    //             sp[sp.length - 2] = "720px";
+    //             location.href = sp.join('/');;
+    //         }
+    //         else {
+    //             sp[sp.length - 2] = "1280px";
+    //             location.href = sp.join('/');;
+    //         }
+    //     }
+    // }
+    // else if (720 <= pixelResolutionWidth) {
+    //     if (pathName != "720px") {
+    //         sp[sp.length - 2] = "720px";
+    //         location.href = sp.join('/');;
+    //     }
+    // }
+    // else {
+    //     if (pathName != "320px") {
+    //         sp[sp.length - 2] = "320px";
+    //         location.href = sp.join('/');;
+    //     }
+    // }
 });
