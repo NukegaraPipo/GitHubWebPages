810a218323ad54ffbde2d901dbd4195cfc542cdd
Atsushi <pipos.pipopo@gmail.com>
2021-02-13 18:42:59 +0900

    jsの処理修正

--- src/common/js/main.js
+++ src/common/js/main.js
@@ -1,43 +1,31 @@
-window.onload = function () {
-
+window.addEventListener('DOMContentLoaded', function () {
     var pixelResolutionWidth = window.screen.width * devicePixelRatio;
-
-    var path = location.pathname;
     var sp = location.href.split("/");
-
-    console.log("path：" + path);
-    console.log("href：" + location.href);
-
     var pathName = sp[sp.length - 2];
-    console.log(pathName);
 
     if (1280 <= pixelResolutionWidth) {
         if (pathName != "1280px") {
             var userAgent = window.navigator.userAgent.toLowerCase();
             if (userAgent.indexOf("mobile") != -1) {
                 sp[sp.length - 2] = "720px";
-                var text = sp.join('/');
-                location.href = text;
+                location.href = sp.join('/');;
             }
             else {
                 sp[sp.length - 2] = "1280px";
-                var text = sp.join('/');
-                location.href = text;
+                location.href = sp.join('/');;
             }
         }
     }
     else if (720 <= pixelResolutionWidth) {
         if (pathName != "720px") {
             sp[sp.length - 2] = "720px";
-            var text = sp.join('/');
-            location.href = text;
+            location.href = sp.join('/');;
         }
     }
     else {
         if (pathName != "320px") {
             sp[sp.length - 2] = "320px";
-            var text = sp.join('/');
-            location.href = text;
+            location.href = sp.join('/');;
         }
     }
-};
+});
